49811628e115c3377c44c55d44c4c97a62dcf3f1
clamp1981 <clamp1981@gmail.com>
2022-06-08 22:43:07 +0900

    feat : set viewbinding

--- app/src/main/java/com/example/mysevenminworkout/MainActivity.kt
+++ app/src/main/java/com/example/mysevenminworkout/MainActivity.kt
@@ -3,15 +3,24 @@ package com.example.mysevenminworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import com.example.mysevenminworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding : ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val btnStart : FrameLayout = findViewById(R.id.flStart )
-        btnStart.setOnClickListener{
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding?.root)
+        binding?.flStart?.setOnClickListener{
 
         }
+
+//        val btnStart : FrameLayout = findViewById(R.id.flStart )
+//        btnStart.setOnClickListener{
+//
+//        }
     }
 }
