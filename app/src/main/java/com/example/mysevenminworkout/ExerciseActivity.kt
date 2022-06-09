05dbc79f517ddd7e647f4ca08d3d69a695e06d2a
clamp1981 <clamp1981@gmail.com>
2022-06-09 22:20:39 +0900

    style : add ExerciseActivity

---
+++ app/src/main/java/com/example/mysevenminworkout/ExerciseActivity.kt
@@ -0,0 +1,16 @@
+package com.example.mysevenminworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mysevenminworkout.databinding.ActivityExerciseBinding
+import com.example.mysevenminworkout.databinding.ActivityMainBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    var binding : ActivityExerciseBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+
+        setContentView(binding?.root)
+    }
+}
