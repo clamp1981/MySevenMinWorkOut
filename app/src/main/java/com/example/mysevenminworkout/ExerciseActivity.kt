65e96efde0f21aef131647b5ff5e46abe4ea4934
clamp1981 <clamp1981@gmail.com>
2022-06-11 22:58:15 +0900

    feat : add rest Timer

--- app/src/main/java/com/example/mysevenminworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/mysevenminworkout/ExerciseActivity.kt
@@ -2,11 +2,14 @@ package com.example.mysevenminworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.example.mysevenminworkout.databinding.ActivityExerciseBinding
 import com.example.mysevenminworkout.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
     var binding : ActivityExerciseBinding? = null
+    var restTimer : CountDownTimer? = null
+    var restTime = 10
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -20,5 +23,40 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        startRestTimer()
+    }
+
+    private fun startRestTimer() {
+        resetRestTimer()
+        setRestTimer()
+    }
+
+    private fun setRestTimer(){
+        var restTimerCount = 0;
+        binding?.progressBar?.progress = restTime;
+        restTimer = object  : CountDownTimer( 1000 * 10 , 1000 ){
+            override fun onTick(p0: Long) {
+                restTimerCount++;
+                binding?.progressBar?.progress = restTime - restTimerCount;
+                binding?.tvTimer?.text = (restTime - restTimerCount).toString()
+            }
+
+            override fun onFinish() {
+                resetRestTimer()
+            }
+        }.start()
+    }
+
+    private fun resetRestTimer(){
+        if( restTimer != null )
+            restTimer?.cancel()
+        restTimer = null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        resetRestTimer()
+        binding = null
     }
 }
