af5c8f7f0e0d71341f1f1a01102acfe007522544
clamp1981 <clamp1981@gmail.com>
2022-06-12 20:13:15 +0900

    feat : set workout Timer

--- app/src/main/java/com/example/mysevenminworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/mysevenminworkout/ExerciseActivity.kt
@@ -9,7 +9,9 @@ import com.example.mysevenminworkout.databinding.ActivityMainBinding
 class ExerciseActivity : AppCompatActivity() {
     var binding : ActivityExerciseBinding? = null
     var restTimer : CountDownTimer? = null
+    var workOutTimer : CountDownTimer? = null
     var restTime = 10
+    var workOutTime = 30
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -34,6 +36,7 @@ class ExerciseActivity : AppCompatActivity() {
 
     private fun setRestTimer(){
         var restTimerCount = 0;
+        binding?.progressBar?.max = restTime
         binding?.progressBar?.progress = restTime;
         restTimer = object  : CountDownTimer( 1000 * 10 , 1000 ){
             override fun onTick(p0: Long) {
@@ -44,6 +47,26 @@ class ExerciseActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 resetRestTimer()
+                binding?.tvTitle?.text = "Exsercise Name"
+                setWorkOutTimer()
+            }
+        }.start()
+    }
+
+
+    private fun setWorkOutTimer(){
+        var workOutTimerCount = 0;
+        binding?.progressBar?.max = workOutTime
+        binding?.progressBar?.progress = workOutTime;
+        workOutTimer = object  : CountDownTimer( 3000 * 10 , 1000 ){
+            override fun onTick(p0: Long) {
+                workOutTimerCount++;
+                binding?.progressBar?.progress = workOutTime - workOutTimerCount;
+                binding?.tvTimer?.text = (workOutTime - workOutTimerCount).toString()
+            }
+
+            override fun onFinish() {
+                resetWorkOutTimer();
             }
         }.start()
     }
@@ -54,9 +77,16 @@ class ExerciseActivity : AppCompatActivity() {
         restTimer = null
     }
 
+    private fun resetWorkOutTimer(){
+        if( workOutTimer != null )
+            workOutTimer?.cancel()
+        workOutTimer = null
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         resetRestTimer()
+        resetWorkOutTimer()
         binding = null
     }
 }
