fd0e7a85cdcb545bc871e0b397da727e5c5fdb2d
clamp1981 <clamp1981@gmail.com>
2022-06-13 10:14:00 +0900

    feat : Link exercise and rest timers

--- app/src/main/java/com/example/mysevenminworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/mysevenminworkout/ExerciseActivity.kt
@@ -3,6 +3,8 @@ package com.example.mysevenminworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
 import com.example.mysevenminworkout.databinding.ActivityExerciseBinding
 import com.example.mysevenminworkout.databinding.ActivityMainBinding
 
@@ -10,10 +12,13 @@ class ExerciseActivity : AppCompatActivity() {
     var binding : ActivityExerciseBinding? = null
     var restTimer : CountDownTimer? = null
     var workOutTimer : CountDownTimer? = null
-    var restTime = 10
-    var workOutTime = 30
     var exerciseArray : ArrayList<ExerciseModel>? = null
+
+    var restTime = 5//10
+    var workOutTime = 10//30
     var currentExerciseIndex = -1
+    var nextExerciseIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -32,7 +37,6 @@ class ExerciseActivity : AppCompatActivity() {
         }
 
 
-
         startRestTimer()
     }
 
@@ -41,40 +45,58 @@ class ExerciseActivity : AppCompatActivity() {
         setRestTimer()
     }
 
+    private fun startWorkOutTimer() {
+        resetWorkOutTimer()
+        setWorkOutTimer()
+    }
+
     private fun setRestTimer(){
+        visiblityExerciseControls(false )
         var restTimerCount = 0;
-        binding?.progressBar?.max = restTime
-        binding?.progressBar?.progress = restTime;
-        restTimer = object  : CountDownTimer( 1000 * 10 , 1000 ){
+        binding?.restProgressBar?.max = restTime
+        binding?.restProgressBar?.progress = restTime;
+        if( nextExerciseIndex < exerciseArray!!.size )
+            binding?.tvNextExerciseName?.text = "The next exercise is the \r\n" + exerciseArray!![nextExerciseIndex].getName()
+
+        restTimer = object  : CountDownTimer( 5000 , 1000 ){
             override fun onTick(p0: Long) {
                 restTimerCount++;
-                binding?.progressBar?.progress = restTime - restTimerCount;
-                binding?.tvTimer?.text = (restTime - restTimerCount).toString()
+                binding?.restProgressBar?.progress = restTime - restTimerCount;
+                binding?.tvRestTimer?.text = (restTime - restTimerCount).toString()
             }
 
             override fun onFinish() {
-                resetRestTimer()
-                currentExerciseIndex++;
-                binding?.tvTitle?.text = exerciseArray!![currentExerciseIndex].getName()
-                setWorkOutTimer()
+                if( currentExerciseIndex < exerciseArray!!.size - 1 ){
+                    currentExerciseIndex++
+                    nextExerciseIndex++
+                    binding?.tvExerciseName?.text = exerciseArray!![currentExerciseIndex].getName()
+                    startWorkOutTimer()
+                }
+
             }
         }.start()
     }
 
 
     private fun setWorkOutTimer(){
+        visiblityExerciseControls(true )
         var workOutTimerCount = 0;
-        binding?.progressBar?.max = workOutTime
-        binding?.progressBar?.progress = workOutTime;
-        workOutTimer = object  : CountDownTimer( 3000 * 10 , 1000 ){
+        binding?.workOutProgressBar?.max = workOutTime
+        binding?.workOutProgressBar?.progress = workOutTime;
+        workOutTimer = object  : CountDownTimer( 10000 , 1000 ){
             override fun onTick(p0: Long) {
                 workOutTimerCount++;
-                binding?.progressBar?.progress = workOutTime - workOutTimerCount;
-                binding?.tvTimer?.text = (workOutTime - workOutTimerCount).toString()
+                binding?.workOutProgressBar?.progress = workOutTime - workOutTimerCount;
+                binding?.tvWorkOutTimer?.text = (workOutTime - workOutTimerCount).toString()
             }
 
             override fun onFinish() {
-                resetWorkOutTimer();
+                if( currentExerciseIndex < exerciseArray!!.size - 1 )
+                    startRestTimer()
+                else{
+                    Toast.makeText(this@ExerciseActivity, "운동 끝", Toast.LENGTH_LONG).show()
+                }
+
             }
         }.start()
     }
@@ -89,6 +111,18 @@ class ExerciseActivity : AppCompatActivity() {
         if( workOutTimer != null )
             workOutTimer?.cancel()
         workOutTimer = null
+    }
+
+    private fun visiblityExerciseControls( isExericse : Boolean ){
+        if( isExericse ){
+            binding?.llRest?.visibility = View.INVISIBLE
+            binding?.llWorkOut?.visibility = View.VISIBLE
+        }else{
+            binding?.llRest?.visibility = View.VISIBLE
+            binding?.llWorkOut?.visibility = View.INVISIBLE
+
+        }
+
     }
 
     override fun onDestroy() {
