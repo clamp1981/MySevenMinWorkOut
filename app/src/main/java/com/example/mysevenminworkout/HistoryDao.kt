7e6f960c1553e4dcef2fbf7416082ee29daea434
clamp1981 <clamp1981@gmail.com>
2022-06-28 22:56:45 +0900

    feat : add HistoryDao.kt and HistoryDatabase.kt

---
+++ app/src/main/java/com/example/mysevenminworkout/HistoryDao.kt
@@ -0,0 +1,18 @@
+package com.example.mysevenminworkout
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface HistoryDao {
+
+    @Insert
+    suspend fun insert( historyEntity: HistoryEntity )
+
+    @Update
+    suspend fun update( historyEntity: HistoryEntity )
+
+    @Query( "SELECT * FROM 'history-table'")
+    fun fetchAllHistories(): Flow<List<HistoryEntity>>
+}
